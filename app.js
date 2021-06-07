fabc53079dad1851d70f1261c973368169ef0093
tsognuh <siciaaatsognuh@gmail.com>
2021-06-08 00:27:34 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ var scores = [0, 0];
 // toglogchiin eeljindee tsugluulj bgaa onoog hadgalah huwisagch
 var roundScore = 0;
 // shoo ali talaaraa buusniig hadgalah huwisagch, 1-6 hoorond sanamsarguigeer uusgej ugnu.
-var dice = Math.floor(Math.random() * 6 + 1);
+var diceNumber = Math.floor(Math.random() * 6 + 1);
 
 // ehlehed beltgeh
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-document.querySelector('.dice').style.display = 'none';
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+var diceDom = document.querySelector('.dice')
+diceDom.style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    var diceNumber = Math.floor(Math.random() * 6 + 1);
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + diceNumber + '.png';
+})
