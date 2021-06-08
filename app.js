7cd7c18c909b1f0b194a52f9d7a15dcf47f32d5a
tsognuh <siciaaatsognuh@gmail.com>
2021-06-08 16:31:22 +0800

    hold button ajillna

--- app.js
+++ app.js
@@ -30,16 +30,33 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
         // ene toglogchiin current onoog 0 bolgono
-        document.getElementById('current-' + activePlayer).textContent = 0;
-        roundScore = 0;
-        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-        // 1 buusan bol toglogchiin eeljiig solij ugnu
-
-        // active tseg arilgana
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        diceDom.style.display = 'none';
-        // if(activePlayer === 0) activePlayer = 1;
-        // else activePlayer = 0;
+        toggleActivePlayer();
     }
 })
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // ug toglogchiin current onoog global onoond nemne
+    scores[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    // ug toglogch hojson esehiig shalgah
+    if (scores[activePlayer] >= 10) {
+        document.getElementById('name-' + activePlayer).textContent = 'WINNER';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        toggleActivePlayer();
+    }
+
+})
+
+function toggleActivePlayer() {
+    document.getElementById('current-' + activePlayer).textContent = 0;
+    roundScore = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    // 1 buusan bol toglogchiin eeljiig solij ugnu
+
+    // active tseg arilgana
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    diceDom.style.display = 'none';
+}
